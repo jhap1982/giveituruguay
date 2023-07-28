47a05fb76b9e2736e9c889a2529c7cdc168b3b5f
jhap1982 <jhap1982@gmail.com>
2023-07-28 17:25:37 -0300

    Some react basic updates.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+            Give It Uruguay!
         </a>
       </header>
     </div>
