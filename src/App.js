2f893286396e8dafb99b7fc762e5fa44940bf69b
jhap1982 <jhap1982@gmail.com>
2023-07-28 22:23:25 -0300

    Adding rhcp logo (https://vectorseek.com/vector_logo/red-hot-chili-peppers-logo-vector/) and change url page (GIU)

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming <code>soon</code>, be patient.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://giveituruguay.blogspot.com"
           target="_blank"
           rel="noopener noreferrer"
         >
