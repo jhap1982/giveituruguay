fae8ce2d73ac53cd3d61f7235791f6cddefe89a5
jhap1982 <jhap1982@gmail.com>
2023-08-01 02:17:53 -0300

    Page styles.

--- src/RepoList.js
+++ src/RepoList.js
@@ -3,23 +3,12 @@ import "./RepoList.css";
 import rotatingSvg from "./logo.svg";
 
 const RepoList = ({ directories }) => {
-  const numPills = directories.length;
-  const pillAngle = (2 * Math.PI) / numPills;
-  const pillRadius = 150;
-
-  const calculatePosition = (index) => {
-    const angle = index * pillAngle;
-    const x = Math.cos(angle) * pillRadius;
-    const y = Math.sin(angle) * pillRadius;
-    return { x, y };
-  };
-
   return (
-    <div>
+    <div className="container">
+      <h1>Give It Uruguay!</h1>
       <div className="logo-container">
         <img src={rotatingSvg} alt="Logo" className="rotating-logo" />
       </div>
-      <h2>Give It Uruguay!</h2>
       <div className="repo-list">
         {directories.map((directory, index) => (
           <a
@@ -28,10 +17,6 @@ const RepoList = ({ directories }) => {
             target="_blank"
             rel="noopener noreferrer"
             className="pill-link"
-            style={{
-              top: `calc(50% + ${calculatePosition(index).y}px)`,
-              left: `calc(50% + ${calculatePosition(index).x}px)`,
-            }}
           >
             {directory}
           </a>
