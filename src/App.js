f6a45385e47607720a6f9b4b5680ef484eccb8f5
jhap1982 <jhap1982@gmail.com>
2023-07-31 23:21:09 -0300

    update project.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
-import React, { useState, useEffect } from 'react';
-
-import './RepoList.css';
+import React from 'react';
+import RepoList from './RepoList';
 import './App.css';
 
-import rotatingSvg from './logo.svg'; // Reemplaza 'path-to-your-svg.svg' con la ruta de tu archivo SVG
+function App() {
+  return (
+    <div className="App">
+      <RepoList />
+    </div>
+  );
+}
 
-const RepoList = () => {
-    const [repoContent, setRepoContent] = useState([]);
-
-    useEffect(() => {
-        // Cambia 'jhap1982' por el nombre de usuario y 'giveituruguay' por el nombre del repositorio
-        const repoUrl = 'https://api.github.com/repos/jhap1982/giveituruguay/contents';
-
-        fetch(repoUrl)
-            .then(response => response.json())
-            .then(data => {
-                // Filtrar solo los elementos que son directorios y no son 'public' o 'src'
-                const filteredContent = data.filter(item => item.type === 'dir' && item.name !== 'public' && item.name !== 'src');
-                setRepoContent(filteredContent);
-            })
-            .catch(error => {
-                console.error('Error fetching repository content:', error);
-            });
-    }, []);
-
-    return (
-        <div className="repo-list-container">
-            <div className="centered-text">
-                <h2>Give It Uruguay!</h2>
-                <img src={rotatingSvg} alt="Rotating SVG" className="rotating-svg" />
-            </div>
-            <div className="pill-container">
-                {repoContent.map(item => (
-                    <a
-                        key={item.name}
-                        href={item.html_url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="pill"
-                    >
-                        <span>{item.name}</span>
-                    </a>
-                ))}
-            </div>
-        </div>
-    );
-};
-
-export default RepoList;
+export default App;
--- src/RepoList.js
+++ src/RepoList.js
@@ -1,74 +1,90 @@
 import React, { useState, useEffect } from 'react';
 import './RepoList.css';
-import rotatingSvg from './logo12122121.svg'; // Reemplaza 'path-to-your-svg.svg' con la ruta de tu archivo SVG
+import logoSvg from './logo.svg'; // Reemplaza 'logo.svg' con la ruta de tu archivo SVG
 
 const RepoList = () => {
-    const [repoContent, setRepoContent] = useState([]);
-    const [selectedFolderContent, setSelectedFolderContent] = useState([]);
+  const [repoContent, setRepoContent] = useState([]);
+  const [selectedFolderContent, setSelectedFolderContent] = useState([]);
+  const [showTable, setShowTable] = useState(false);
 
-    useEffect(() => {
-        // Cambia 'jhap1982' por el nombre de usuario y 'giveituruguay' por el nombre del repositorio
-        const repoUrl = 'https://api.github.com/repos/jhap1982/giveituruguay/contents';
+  useEffect(() => {
+    // Cambia 'jhap1982' por el nombre de usuario y 'giveituruguay' por el nombre del repositorio
+    const repoUrl = 'https://api.github.com/repos/jhap1982/giveituruguay/contents';
 
-        fetch(repoUrl)
-            .then(response => response.json())
-            .then(data => {
-                // Filtrar solo los elementos que son directorios y no son 'public' o 'src'
-                const filteredContent = data.filter(item => item.type === 'dir' && item.name !== 'public' && item.name !== 'src');
-                setRepoContent(filteredContent);
-            })
-            .catch(error => {
-                console.error('Error fetching repository content:', error);
-            });
-    }, []);
+    fetch(repoUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Filtrar solo los elementos que son directorios y no son 'public' o 'src'
+        const filteredContent = data.filter(item => item.type === 'dir' && item.name !== 'public' && item.name !== 'src');
+        setRepoContent(filteredContent);
+      })
+      .catch(error => {
+        console.error('Error fetching repository content:', error);
+      });
+  }, []);
 
-    const handlePillClick = (folderName) => {
-        // Obtener el contenido de la carpeta seleccionada por el nombre
-        const selectedFolder = repoContent.find(item => item.name === folderName);
-        if (selectedFolder) {
-            const folderUrl = selectedFolder.url;
-            fetch(folderUrl)
-                .then(response => response.json())
-                .then(data => {
-                    // Filtrar solo los elementos que son directorios dentro de la carpeta seleccionada
-                    const folderContent = data.filter(item => item.type === 'dir');
-                    setSelectedFolderContent(folderContent);
-                })
-                .catch(error => {
-                    console.error('Error fetching selected folder content:', error);
-                });
-        }
-    };
+  const handlePillClick = async (folderName) => {
+    const selectedFolder = repoContent.find(item => item.name === folderName);
+    if (selectedFolder) {
+      const folderUrl = selectedFolder.url;
+      const response = await fetch(folderUrl);
+      const data = await response.json();
+      const folderContent = data.filter(item => item.type === 'dir');
+      setSelectedFolderContent(folderContent);
+      setShowTable(true);
+    }
+  };
 
-    return (
-        <div className="repo-list-container">
-            <div className="centered-text">
-                <h2>Give It Uruguay!</h2>
-                <img src={rotatingSvg} alt="Rotating SVG" className="rotating-svg" />
+  const handleBackButtonClick = () => {
+    setShowTable(false);
+  };
+
+  return (
+    <div className="repo-list-container">
+      <div className="centered-text">
+        <h2>Give It Uruguay!</h2>
+        <img src={logoSvg} alt="Logo SVG" className="logo-svg" />
+      </div>
+      {!showTable ? (
+        <div className="pill-container">
+          {repoContent.map(item => (
+            <div
+              key={item.name}
+              className="pill"
+              onClick={() => handlePillClick(item.name)}
+            >
+              <span>{item.name}</span>
             </div>
-            <div className="pill-container">
-                {repoContent.map(item => (
-                    <div
-                        key={item.name}
-                        className="pill"
-                        onClick={() => handlePillClick(item.name)}
-                    >
-                        <span>{item.name}</span>
-                    </div>
-                ))}
-            </div>
-            {selectedFolderContent.length > 0 && (
-                <div className="selected-folder-container">
-                    <h3>Contenido de la carpeta seleccionada:</h3>
-                    <ul>
-                        {selectedFolderContent.map(item => (
-                            <li key={item.name}>{item.name}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+          ))}
         </div>
-    );
+      ) : (
+        <div className="selected-folder-container">
+          <button onClick={handleBackButtonClick} className="volver-btn">
+            BACK
+          </button>
+          <h3>Tour</h3>
+          {selectedFolderContent.length > 0 ? (
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedFolderContent.map(item => (
+                  <tr key={item.name}>
+                    <td>{item.name}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>Nothing found.</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RepoList;
