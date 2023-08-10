a204ae4cca677657412b7a36aee4fdd5f51d984b
jhap1982 <jhap1982@gmail.com>
2023-08-10 09:42:34 -0300

    What i have 'till 1985

--- src/WhatIHave.js
+++ src/WhatIHave.js
@@ -4,11 +4,15 @@ import './WhatIHave.css';
 const WhatIHave = ({ onBack }) => {
     return (
         <div className="what-i-have-container">
+
             <button className="back-button" onClick={onBack}>
                 Back
             </button>
 
-            <h2>What I Have</h2>
+            <section>
+                <h3>1982</h3>
+                nope
+            </section>
 
             <section>
                 <h3>1983</h3>
@@ -61,6 +65,552 @@ const WhatIHave = ({ onBack }) => {
                             <td>1984-12-16 - The Bowery, Oklahoma City, OK, USA</td>
                             <td>[AUD #1][FLAC]</td>
                         </tr>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1985</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1985-01-06 - Club Culture, Santa Cruz, CA, USA</td>
+                            <td>[AUD #1][FLAC]</td>
+                        </tr>
+
+                        <tr>
+                            <td>1985-06-28 - Mason Jar, Phoenix, AZ, USA</td>
+                            <td>[AUD #1][FLAC]</td>                            
+                        </tr>
+
+                        <tr>
+                            <td>1985-07-06 - The Keystone, Palo Alto, CA, USA</td>
+                            <td>[AUD #1][SCB][FLAC]</td>
+                        </tr>
+
+                        <tr>
+                            <td>1985-08-17 - Freilichtbuhne Loreley (Loreley Open Air), St. Goarshausen, Germany</td>
+                            <td>[PRO #1][George Clinton][DVD] [PRO #1][RHL][DVD]</td>
+                        </tr>
+
+                        <tr>
+                            <td>1985-10-12 - The Keystone, Palo Alto, CA, USA</td>
+                            <td>[AUD #1][FLAC]</td>
+                        </tr>
+
+                        <tr>
+                            <td>1985-10-23 - The Patio, Indianapolis, IN, USA</td>
+                            <td>[AMT #1][BONG][DVD] [AMT #1][RHL][DVD] [AUD #1][FLAC]</td>
+                        </tr>
+
+                        <tr>
+                            <td>1985-11-29 - Roosters, Nashville, TN, USA</td>
+                            <td>[AUD #1][FLAC]</td>
+                        </tr>                        
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1986</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1987</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1988</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1989</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1990</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1991</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1992</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1993</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1994</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1995</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+
+            <section>
+                <h3>1996</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1997</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1998</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>1999</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2000</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+
+            <section>
+                <h3>2001</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2002</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2003</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2004</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2005</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2006</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2007</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2008</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2009</h3>
+                nope
+            </section>
+
+            <section>
+                <h3>2010</h3>
+                nope
+            </section>
+
+            <section>
+                <h3>2011</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2012</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2013</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2014</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2015</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2016</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2017</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2018</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2019</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2020</h3>
+                nope
+            </section>
+
+            <section>
+                <h3>2021</h3>
+                nope
+            </section>
+
+            <section>
+                <h3>2022</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h3>2023</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Concert</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                     </tbody>
                 </table>
             </section>
